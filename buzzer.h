9dd1388441362aa49938615493eb5b612a49922a
Matt Wawiorka <30660814+mwawiorka@users.noreply.github.com>
2018-04-25 08:57:25 -0500

    Added notes

--- buzzer.h
+++ buzzer.h
@@ -3,10 +3,29 @@
 
 #include "pwm.h"
 
+#define C		523
+#define C_s		554
+#define D		587
+#define D_s		622
+#define	E		659
+#define	F		698
+#define	F_s		740
+#define	G		784
+#define	G_s		831
+#define	A		880
+#define	A_s		932
+#define	B		988
+
 /******************************************************************************
  * Sends PWM signal to buzzer
  *
  *****************************************************************************/
 void buzz( int frequency );
+
+/******************************************************************************
+ * Stops PWM signal to buzzer
+ *
+ *****************************************************************************/
+void stop_buzz( );
 
 #endif
