0292c98678762a5440edf256b9e0facd8320dec5
Dan-Bai <dbai5@wisc.edu>
2018-05-02 17:16:36 -0500

    Add files via upload

---
+++ paused.h
@@ -0,0 +1,12 @@
+#ifndef __PAUSED_H__
+#define __PAUSED_H__
+
+#include <stdint.h>
+#include <stdio.h>
+
+#define       	PAUSED_HEIGHT	243
+#define       	PAUSED_WIDTH  	56
+
+extern const uint8_t pausedBitmaps[];
+
+#endif
---
+++ title.h
@@ -0,0 +1,12 @@
+#ifndef __TITLE_H__
+#define __TITLE_H__
+
+#include <stdint.h>
+#include <stdio.h>
+
+#define       	TITLE_HEIGHT	259
+#define       	TITLE_WIDTH  	112
+
+extern const uint8_t titleBitmaps[];
+
+#endif
