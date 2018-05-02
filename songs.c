1b418a90d97c7d3c47b8e83974b4cc3dbfc76a12
Matt Wawiorka <30660814+mwawiorka@users.noreply.github.com>
2018-05-02 16:52:32 -0500

    Update songs.c

--- songs.c
+++ songs.c
@@ -7,7 +7,6 @@
 const key_t maryHadALittleLamb[] =
 {
 	Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil,
-	Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil,
 	En, Cont, Cont, Cont, Dn, Cont, Cont, Cont, Cn, Cont, Cont, Cont, Dn, Cont, Cont, Cont,
 	En, Cont, Cont, Cont, En, Cont, Cont, Cont, En, Cont, Cont, Cont, Cont, Cont, Cont, Cont,
 	Dn, Cont, Cont, Cont, Dn, Cont, Cont, Cont, Dn, Cont, Cont, Cont, Cont, Cont, Cont, Cont,
@@ -16,5 +15,37 @@ const key_t maryHadALittleLamb[] =
 	En, Cont, Cont, Cont, En, Cont, Cont, Cont, En, Cont, Cont, Cont, Cn, Cont, Cont, Cont,
 	Dn, Cont, Cont, Cont, Dn, Cont, Cont, Cont, En, Cont, Cont, Cont, Dn, Cont, Cont, Cont,
 	Cn, Cont, Cont, Cont, Cont, Cont, Cont, Cont, Cont, Cont, Cont, Cont, Cont, Cont, Cont, Cont,
+	En, Cont, Cont, Cont, Dn, Cont, Cont, Cont, Cn, Cont, Cont, Cont, Dn, Cont, Cont, Cont,
+	En, Cont, Cont, Cont, En, Cont, Cont, Cont, En, Cont, Cont, Cont, Cont, Cont, Cont, Cont,
+	Dn, Cont, Cont, Cont, Dn, Cont, Cont, Cont, Dn, Cont, Cont, Cont, Cont, Cont, Cont, Cont,
+	En, Cont, Cont, Cont, En, Cont, Cont, Cont, En, Cont, Cont, Cont, Cont, Cont, Cont, Cont,
+	En, Cont, Cont, Cont, Dn, Cont, Cont, Cont, Cn, Cont, Cont, Cont, Dn, Cont, Cont, Cont,
+	En, Cont, Cont, Cont, En, Cont, Cont, Cont, En, Cont, Cont, Cont, Cn, Cont, Cont, Cont,
+	Dn, Cont, Cont, Cont, Dn, Cont, Cont, Cont, En, Cont, Cont, Cont, Dn, Cont, Cont, Cont,
+	Cn, Cont, Cont, Cont, Cont, Cont, Cont, Cont, Cont, Cont, Cont, Cont, Cont, Cont, Cont, Cont,
+	Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil,
 	End
 };	
+
+const key_t pokemonTheme[] =
+{
+	Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil,
+	Sil, Sil, An, Cont, An, Cont, An, Cont, An, Cont, Cont, Cont, Cont, Cont, An, Cont,
+	Gn, Cont, Cont, Cont, En, Cont, Cn, Cont, Cont, Cont, Cont, Cont, Cont, Cont, Cn, Cont,
+	An, Cont, Cont, Cont, An, Cont, Cont, Cont, Gn, Cont, Fn, Cont, Gn, Cont, Cont, Cont,
+	Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil,
+	Sil, Sil, Fn, Cont, As, Cont, Cont, Cont, As, Cont, Cont, Cont, As, Cont, An, Cont,
+	Cont, Cont, Gn, Cont, Cont, Cont, Fn, Cont, Cont, Cont, Cont, Cont, Cont, Cont, Fn, Cont,
+	An, Cont, Cont, Cont, An, Cont, Gn, Cont, Cont, Cont, Fn, Cont, An, Cont, Cont, Cont,
+	Cont, Cont, Cont, Cont,	Cont, Cont, Cont, Cont, Cont, Cont, Cont, Cont, Cont, Cont, Cont, Cont,
+	Sil, Sil, An, Cont, An, Cont, An, Cont, An, Cont, Cont, Cont, Cont, Cont, An, Cont,
+	Gn, Cont, Cont, Cont, En, Cont, Cn, Cont, Cont, Cont, Cont, Cont, Cont, Cont, Cn, Cont,
+	An, Cont, Cont, Cont, An, Cont, Cont, Cont, Gn, Cont, Fn, Cont, Gn, Cont, Cont, Cont,
+	Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil,
+	Sil, Sil, Fn, Cont, As, Cont, Cont, Cont, As, Cont, Cont, Cont, As, Cont, An, Cont,
+	Cont, Cont, Gn, Cont, Cont, Cont, Fn, Cont, Cont, Cont, Cont, Cont, Cont, Cont, Fn, Cont,
+	An, Cont, Cont, Cont, An, Cont, Gn, Cont, Cont, Cont, Fn, Cont, An, Cont, Cont, Cont,
+	Cont, Cont, Cont, Cont,	Cont, Cont, Cont, Cont, Cont, Cont, Cont, Cont, Cont, Cont, Cont, Cont,
+	Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil, Sil,
+	End
+};
