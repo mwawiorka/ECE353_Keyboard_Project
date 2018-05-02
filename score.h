d0f4f32b8510027a909242311f203f8e7450376c
Dan-Bai <dbai5@wisc.edu>
2018-05-02 17:26:23 -0500

    Add files via upload

---
+++ score.h
@@ -0,0 +1,10 @@
+#ifndef __PROJECT_IMAGES_H__
+#define __PROJECT_IMAGES_H__
+
+#include <stdint.h>
+#include <stdio.h>
+
+extern const uint8_t numbersBitmaps[];
+extern const uint16_t numbersDescriptors[];
+
+#endif
