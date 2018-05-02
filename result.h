3795b0bfab980207b82e1015a5d43ef0ed7519d1
Dan-Bai <dbai5@wisc.edu>
2018-05-02 17:42:19 -0500

    Add files via upload

---
+++ result.h
@@ -0,0 +1,12 @@
+#ifndef __RESULT_H__
+#define __RESULT_H__
+
+#include <stdint.h>
+#include <stdio.h>
+
+#define       	RESULT_WIDTH 	48
+#define       	RESULT_HEIGHT  	207	
+
+extern const uint8_t resultBitmap[];
+
+#endif
