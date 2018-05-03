145d8de93d0d1d7c6a1731fd8e666c267075c0c2
Dan-Bai <dbai5@wisc.edu>
2018-05-03 00:15:05 -0500

    Update Menu_bitmap.h

--- Menu_bitmap.h
+++ Menu_bitmap.h
@@ -4,14 +4,13 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#define       	MENU1 	60
-#define       	MENU2  	120		
-#define       	MENU3  	180	
-#define			MENUROW	280
+#define       	MENU1 	60		// Line 1 of display menu selection
+#define       	MENU2  	120		// Line 2 of display menu selection
+#define       	MENU3  	180		// Line 3 of display menu selection
+#define			MENUROW	280		// The way left location of names
 
 extern const uint16_t letterRedirect[];
 extern const uint8_t letterBitmaps[];
 extern const uint8_t selectBoxHor[];
-extern const uint8_t arrowHor[];
 
 #endif
